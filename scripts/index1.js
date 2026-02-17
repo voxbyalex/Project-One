672c2277c4176e6e97c12f0613d81def86d1f139
voxbyalex <marvincortez-estudiant@gmail.com>
2026-02-16 20:03:11 -0600

    update

--- scripts/index1.js
+++ scripts/index1.js
@@ -19,5 +19,18 @@ function typeEffect() {
     }, 2500);
   }
 }
-
 typeEffect();
+
+const content = document.querySelector(".content");
+
+setTimeout(() => {
+  content.classList.add("mostrar");
+}, 3600);
+
+const emailInput = document.getElementById("email");
+emailInput.addEventListener("invalid", () => {
+  emailInput.setCustomValidity("Please enter a valid email address");
+});
+emailInput.addEventListener("input", () => {
+  emailInput.setCustomValidity("");
+});
